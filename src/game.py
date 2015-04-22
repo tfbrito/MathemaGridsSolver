afd463d920ee9ed7d07dcbdfbea3e77b98d5c918
H3llT0uCh <tiago.brito3@gmail.com>
2015-04-22 15:16:37 +0100

    [Feature] Basic Structure of menu #11

--- src/game.py
+++ src/game.py
@@ -1,11 +1,55 @@
 # -*- coding: UTF-8 -*-
 import sys
 
-print 'Number of arguments:', len(sys.argv), 'arguments.'
-print 'Argument List:', str(sys.argv)
+filename = None
+
+def solve():
+	print filename
+	print "Let's solve the puzzle!"
+
+def generate():
+    print "Generate a puzzle"
+
+def play():
+   	print "Let's play a game!"
+
+def exit():
+	print "Exiting...."
+	sys.exit(1)
+
+options = {1 : solve,
+           2 : generate,
+           3 : play,
+           9 : exit,
+}
+
+def menu():
+	flag = True
+	print "-------------------------------------"
+	print "Welcome to MathemaGrids Puzzle Solver"
+	print "-------------------------------------"
+	print "\n"
+	print "----------------"
+	print "|     Menu     |"
+	print "----------------"
+	print "1 - Solve Puzzle from file"
+	print "2 - Generate puzzle"
+	print "3 - Play puzzle"
+	print "9 - Exit"
+	while(flag):
+		var = input("What is your choise? ")
+		if(var in options):
+			flag = False
+			options[var]()
+		else:
+			print "Wrong choise, please try again!"
 
 if(len(sys.argv)==1):
 	import gameGui
 	gameGui.MathemaGridsApp().run()
 elif(sys.argv[1] == "--console"):
-	print "execute console version"
+	if(len(sys.argv)>2):
+		filename = sys.argv[2]
+		menu()
+	else:
+		print "Missing input file!"
